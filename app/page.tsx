37add7f7b90d7b3fb2877a0c70c777c0c82d270a
2004khushi <goyalkhushi1004@gmail.com>
2025-12-29 08:50:35 +0530

    implement navigation

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Page = () => {
     return (
-        <div className="text-3xl text-indigo-500">Page</div>
+        <div>
+
+        </div>
     )
 }
 export default Page
